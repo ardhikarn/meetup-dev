88526230388cbcbb2f118062211b98494f978f17
ardhikarn <ardhikarn@gmail.com>
2021-01-12 00:34:20 +0700

    setting prettier and vuetify: custom color

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib/framework'
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  icons: {
+    iconfont: 'mdiSvg' || 'md'
+  },
+  theme: {
+    themes: {
+      light: {
+        primary: colors.indigo.darken4, // #1A237E
+        secondary: colors.red.lighten4, // #FFCDD2
+        accent: colors.indigo.base // #3F51B5
+      }
+    }
+  }
 })
